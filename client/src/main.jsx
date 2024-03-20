1cf167a77c059dc4da4706503a08f56578662600
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-03-20 10:38:22 -0700

    add browserrouter & apolloClient shell

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserRouter } from 'react-router-dom'
+
+// import {} from ''
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+const client = new ApolloClient({
+  uri: 'graphql',
+  cache: new InMemoryCache(),
+})
+
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider>
+        <App />
+
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
