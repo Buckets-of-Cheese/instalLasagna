9d89ee2f5888c9a780d17406316801f63397f1a3
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-03-21 14:44:11 -0400

    work in progress on models

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,7 +1,30 @@
 const { model, Schema } = require('mongoose')
 
 const recipeSchema = new Schema(
-
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: { type: String },
+        environment: {
+            type: String,
+            enum: ['Mac OS','Windows','Linux','iOS', 'Android', 'Other'],
+            default: 'Other'
+        },
+        creatorID: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        steps: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Steps'
+        }],
+        private: {
+            type: Boolean,
+            default: false
+        }
+    }
 )
 
 const Recipe = model('Recipe', recipeSchema)
